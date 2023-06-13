433e165ae602fa20088b0b2a6a52a6923f2ca4fa
Luiz Paulo Martins dos Santos <117921451+LuizPaulo1990@users.noreply.github.com>
2023-06-13 16:45:33 -0300

    mudanças media

--- botao-menu-modelo-portatil.js
+++ botao-menu-modelo-portatil.js
@@ -4,8 +4,8 @@ let iconeFechaMenu = document.querySelector('#butao-fecha-menu');
 
 let iconeBarraNavegação = document.querySelector('.barra-navegacao');
 
-console.log(iconeMenu);
-console.log(iconeBarraNavegação);
+let largura =  document.documentElement.clientWidth;
+
 
 iconeMenu.addEventListener('click',() => {
     iconeBarraNavegação.style.display='block';
@@ -13,6 +13,11 @@ iconeMenu.addEventListener('click',() => {
     iconeFechaMenu.style.display = 'inline';
 })
 
+iconeBarraNavegação.addEventListener('click',() =>{
+    iconeBarraNavegação.style.display='none';
+    iconeFechaMenu.style.display = 'none';
+    iconeMenu.style.display ='inline';
+})
 
 iconeFechaMenu.addEventListener('click',() =>{
     iconeBarraNavegação.style.display='none';
