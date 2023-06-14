fac57d01b7c6013181003b89453ddda057333eea
Luiz Paulo Martins dos Santos <117921451+LuizPaulo1990@users.noreply.github.com>
2023-06-14 11:17:53 -0300

    alterações javascript

--- botao-menu-modelo-portatil.js
+++ botao-menu-modelo-portatil.js
@@ -4,23 +4,44 @@ let iconeFechaMenu = document.querySelector('#butao-fecha-menu');
 
 let iconeBarraNavegação = document.querySelector('.barra-navegacao');
 
-let largura =  document.documentElement.clientWidth;
+
+function checaLargura(largura) {
+    if (largura < 771) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
-iconeMenu.addEventListener('click',() => {
-    iconeBarraNavegação.style.display='block';
-    iconeMenu.style.display ='none';
-    iconeFechaMenu.style.display = 'inline';
-})
+function abreMenu() {
+    let largura = document.documentElement.clientWidth;
+    if (checaLargura(largura)) {
+        iconeBarraNavegação.style.display = 'block';
+        iconeMenu.style.display = 'none';
+        iconeFechaMenu.style.display = 'inline';
+    }
+}
+iconeMenu.addEventListener('click', abreMenu);
 
-iconeBarraNavegação.addEventListener('click',() =>{
-    iconeBarraNavegação.style.display='none';
-    iconeFechaMenu.style.display = 'none';
-    iconeMenu.style.display ='inline';
-})
+function menuNavegação() {
+    let largura = document.documentElement.clientWidth;
+    if (checaLargura(largura)) {
+        iconeBarraNavegação.style.display = 'none';
+        iconeFechaMenu.style.display = 'none';
+        iconeMenu.style.display = 'inline';
 
-iconeFechaMenu.addEventListener('click',() =>{
-    iconeBarraNavegação.style.display='none';
-    iconeMenu.style.display ='inline';
-    iconeFechaMenu.style.display = 'none';
-})
+    }
+}
+iconeBarraNavegação.addEventListener('click', menuNavegação)
+
+
+function fechaMenu() {
+    let largura = document.documentElement.clientWidth;
+    if (checaLargura(largura)) {
+        iconeBarraNavegação.style.display = 'none';
+        iconeMenu.style.display = 'inline';
+        iconeFechaMenu.style.display = 'none';
+    }
+}
+iconeFechaMenu.addEventListener('click', fechaMenu);
