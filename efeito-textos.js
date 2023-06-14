e59cca9d3e92f06bc6bf04af27b930435784a67c
Luiz Paulo Martins dos Santos <117921451+LuizPaulo1990@users.noreply.github.com>
2023-06-14 19:54:58 -0300

    add coisas

--- efeito-textos.js
+++ efeito-textos.js
@@ -16,12 +16,15 @@ ScrollReveal().reveal('.texto-contato1, .P1, .P4, .habilidade1, .habilidade3', {
 
 ScrollReveal().reveal('.texto-contato2, .P3, .P6, .habilidade2, .habilidade4', { origin: 'right' });
 
+
+
+
 /*============= efeito de escrever e apagar o texto selecionado=====================*/
 
-const typed = new Typed('#multiple-text', {
-    strings: ['Desenvolvedor Frontend'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
+// const typed = new Typed('#multiple-text', {
+//     strings: ['Desenvolvedor Frontend'],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// })
